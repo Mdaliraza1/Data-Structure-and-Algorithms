f237cc9609890b3e06a152c6d63c526975ef3fe6
Md Ali Raza <mdaliraza92@gmail.com>
2025-12-01 15:23:31 +0530

    Stock buy and sell profit

---
+++ Arrays/StockProfit.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class StockProfit {
+    public static int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minPrice = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+            if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String[] args) {
+        int[] prices = {7, 6, 4, 3, 1};
+        System.out.println(maxProfit(prices));
+    }
+}
